fc53171699647b739933da7c32f7e981341b2ba0
Latrell26 <cruzlatrell5@gmail.com>
2026-07-20 23:42:45 +0800

    chore: build main

---
+++ .history/src/App_20260720233638.jsx
@@ -0,0 +1,11 @@
+import ReportIssueForm from "./ReportIssueForm";
+
+function App() {
+  return (
+    <main className="min-h-screen bg-[#FBFAF7] px-4 py-10 sm:px-6 sm:py-14 flex justify-center">
+      <ReportIssueForm />
+    </main>
+  );
+}
+
+export default App;
